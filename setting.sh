35ac31ff84729acef5c3fc9a826b40a23ceb84d4
jacky shi <102337558+jackyandszy@users.noreply.github.com>
2025-11-03 22:12:51 +0800

    更新 setting.sh

--- setting.sh
+++ setting.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '1i /plugin/;' arch/${{ env.ARCH }}/configs/${{ env.KERNEL_CONFIG }}
-sed -i '1i /dts-v1/;' arch/${{ env.ARCH }}/configs/${{ env.KERNEL_CONFIG }}
+sed -i '1i /plugin/;' arch/arm64/boot/dts/qcom/cepheus-pinctrl.dtsi
+sed -i '1i /dts-v1/;' arch/arm64/boot/dts/qcom/cepheus-pinctrl.dtsi
