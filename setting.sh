c2cc2bad0843d165eab2282e3c0b6dfb1a7ff911
jacky shi <102337558+jackyandszy@users.noreply.github.com>
2025-11-08 23:57:38 +0800

    更新 setting.sh

--- setting.sh
+++ setting.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#cd $GITHUB_WORKSPACE/
+cd $GITHUB_WORKSPACE/
 #mv -f ${{ env.KERNEL_CONFIG }} $GITHUB_WORKSPACE/kernel_workspace/android-kernel/arch/${{ env.ARCH }}/configs/
+#mv -f dtc $GITHUB_WORKSPACE/kernel_workspace/toolchain/bin/
